47bbd480f478a8f7f75ef634e7763f3999edb2e5
Andrew <aeworth@emory.edu>
2024-04-09 16:20:39 -0400

    Update emg_transform.py
    Added new helper function for calculating mean and std of reflex values. May need some refactoring later to clean up the mess in the function parameters...

--- emg_transform.py
+++ emg_transform.py
@@ -1,5 +1,9 @@
 # emg_transform.py
 
+"""
+Helper functions for EMG calculations and transformations.
+"""
+
 import numpy as np
 
 def rectify_emg(emg_array):
@@ -17,3 +21,23 @@ def calculate_average_amplitude(emg_data, start_ms, end_ms, scan_rate):
     emg_window = emg_data[start_index:end_index]
     rectified_emg_window = rectify_emg(emg_window)
     return np.mean(rectified_emg_window)
+
+def calculate_mean_std(recordings, stimulus_value, channel_index, m_start_ms, m_end_ms, h_start_ms, h_end_ms, bin_size, scan_rate):
+    """
+    Calculate the M-responses and H-reflexes over multiple sessions for a given stimulus voltage, binning the stimulus voltages.
+    """
+    m_wave_amplitudes = []
+    h_response_amplitudes = []
+    for recording in recordings:
+        binned_stimulus_v = round(recording['stimulus_v'] / bin_size) * bin_size
+        if binned_stimulus_v == stimulus_value:
+            channel_data = recording['channel_data'][channel_index]
+            m_wave_amplitude = calculate_average_amplitude(channel_data, m_start_ms, m_end_ms, scan_rate)
+            h_response_amplitude = calculate_average_amplitude(channel_data, h_start_ms, h_end_ms, scan_rate)
+            m_wave_amplitudes.append(m_wave_amplitude)
+            h_response_amplitudes.append(h_response_amplitude)
+    m_wave_mean = np.mean(m_wave_amplitudes)
+    m_wave_std = np.std(m_wave_amplitudes)
+    h_response_mean = np.mean(h_response_amplitudes)
+    h_response_std = np.std(h_response_amplitudes)
+    return m_wave_mean, m_wave_std, h_response_mean, h_response_std
