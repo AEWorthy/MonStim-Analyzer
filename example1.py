e3a093476ff9c43fef5205b96e290e09983d287e
Andrew <aeworth@emory.edu>
2024-04-16 13:19:11 -0400

    Misc. updates

---
+++ example1.py
@@ -0,0 +1,34 @@
+# MonStim_CSV_Analysis - example1.py
+
+import csv_to_pickle
+import EMG_Utils
+
+DATA_PATH = 'files_to_analyze'
+OUTPUT_PATH = 'output'
+
+ex_sciatic_stim_session = 'output/240404-4_data.pickle'
+ex_tibial_stim_session = 'output/240404-11_data.pickle'
+ex_cper_stim_session = 'output/240404-10_data.pickle'
+
+sciatic_dataset = ['output/240404-1_data.pickle','output/240404-2_data.pickle','output/240404-3_data.pickle','output/240404-4_data.pickle','output/240404-5_data.pickle']
+tibial_dataset = ['output/240404-11_data.pickle','output/240404-12_data.pickle','output/240404-13_data.pickle','output/240404-14_data.pickle','output/240404-15_data.pickle']
+cper_dataset = ['output/240404-6_data.pickle','output/240404-8_data.pickle','output/240404-9_data.pickle','output/240404-10_data.pickle']
+
+## Process CSV files in "\files_to_analyze" into pickle files formatted for downstream analysis.
+# csv_to_pickle.pickle_dataset(DATA_PATH, OUTPUT_PATH)
+
+## Analysis for a single session using class-based method
+session = EMG_Utils.EMGSession(ex_sciatic_stim_session)
+session.session_parameters()
+session.plot_emg(channel_names=["LG", "TA"])
+session.plot_emg_rectified(channel_names=["LG", "TA"], m_flags=True, h_flags=True)
+# session.plot_emg_suspectedH(channel_names=["LG", "TA"], h_threshold=0.05)
+# session.plot_reflex_curves(channel_names=["LG", "TA"])
+
+## Analysis for multiple sessions.
+sessions = []
+for session in sciatic_dataset:
+    sessions.append(EMG_Utils.EMGSession(session))
+
+dataset = EMG_Utils.EMGDataset(sessions)
+dataset.plot_reflex_curves(channel_names=["LG", "TA"])
--- main.py
+++ main.py
@@ -6,29 +6,26 @@ import EMG_Utils
 DATA_PATH = 'files_to_analyze'
 OUTPUT_PATH = 'output'
 
-ex_sciatic_stim_session = 'output/240404-4_data.pickle'
-ex_tibial_stim_session = 'output/240404-11_data.pickle'
-ex_cper_stim_session = 'output/240404-10_data.pickle'
+pickled_test_session = 'output/240304 mcurve4 ii_data.pickle' #'output/040224rec1_data.pickle'
+# pickled_test_dataset = ['output/240304 hcurve1_data.pickle','output/240304 hcurve2_data.pickle','output/240304 h3_data.pickle','output/240304 h4 1k_data.pickle']
+pickled_test_dataset = ['output/240304 mcurve2 ii_data.pickle','output/240304 mcurve3 ii_data.pickle','output/240304 mcurve4 ii_data.pickle']
 
-sciatic_dataset = ['output/240404-1_data.pickle','output/240404-2_data.pickle','output/240404-3_data.pickle','output/240404-4_data.pickle','output/240404-5_data.pickle']
-tibial_dataset = ['output/240404-11_data.pickle','output/240404-12_data.pickle','output/240404-13_data.pickle','output/240404-14_data.pickle','output/240404-15_data.pickle']
-cper_dataset = ['output/240404-6_data.pickle','output/240404-8_data.pickle','output/240404-9_data.pickle','output/240404-10_data.pickle']
 
 ## Process CSV files in "\files_to_analyze" into pickle files formatted for downstream analysis.
 # csv_to_pickle.pickle_dataset(DATA_PATH, OUTPUT_PATH)
 
 ## Analysis for a single session using class-based method
-session = EMG_Utils.EMGSession(ex_sciatic_stim_session)
+session = EMG_Utils.EMGSession(pickled_test_session)
 session.session_parameters()
-session.plot_emg(channel_names=["LG", "TA"])
-session.plot_emg_rectified(channel_names=["LG", "TA"], m_flags=True, h_flags=True)
-# session.plot_emg_suspectedH(channel_names=["LG", "TA"], h_threshold=0.05)
-# session.plot_reflex_curves(channel_names=["LG", "TA"])
+session.plot_emg(channel_names=["LG"], m_flags=True, h_flags=True)
+session.plot_emg_rectified(channel_names=["LG"], m_flags=True, h_flags=True)
+# session.plot_emg_suspectedH(channel_names=["LG"], h_threshold=0.05)
+session.plot_reflex_curves(channel_names=["LG"])
 
 ## Analysis for multiple sessions.
 sessions = []
-for session in sciatic_dataset:
+for session in pickled_test_dataset:
     sessions.append(EMG_Utils.EMGSession(session))
 
 dataset = EMG_Utils.EMGDataset(sessions)
-dataset.plot_reflex_curves(channel_names=["LG", "TA"])
+dataset.plot_reflex_curves(channel_names=["LG"])
