3ed94d0ee763ede052916b9fddfa7ab11176c8b5
AEWorthy <aeworth@emory.edu>
2024-05-28 12:43:36 -0400

    added top-level init

---
+++ __init__.py
@@ -0,0 +1,15 @@
+# monstim_to_pickle/__init__.py
+
+__version__ = '1.0.0'
+
+# Metadata
+__title__ = 'monstim_analysis'
+__description__ = 'Convert MonStim CSV files to Pickle files.'
+__author__ = 'Andrew Worthy'
+__email__ = 'aeworth@emory.edu'
+__license__ = 'MIT'
+
+# Import functions
+
+
+# Define __all__ for module
